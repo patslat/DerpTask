334f09ac5fefd473c682e328c391eedcd4fef811
Patrick Slattery <slattery.patrickj@gmail.com>
2013-07-01 11:24:37 -0700

    basic projects sidebar

--- app/assets/javascripts/drop_task.js
+++ app/assets/javascripts/drop_task.js
@@ -3,11 +3,18 @@ window.DropTask = {
   Collections: {},
   Views: {},
   Routers: {},
-  initialize: function() {
+  initialize: function ($content, $sidebar, data) {
     var projects = new DropTask.Collections.Projects(data.projects)
+    this.installSidebar($sidebar, projects)
+
+  },
+
+  installSidebar: function ($sidebar, projects) {
+    var that = this;
+    var projectsView = new DropTask.Views.ProjectsIndex({
+      collection: projects
+    });
+
+    $sidebar.html(projectsView.render().$el);
   }
 };
-
-$(document).ready(function(){
-  DropTask.initialize();
-});
