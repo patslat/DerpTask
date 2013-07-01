40a1d6a940975d51b066848c60dd119521c9caff
Patrick Slattery <slattery.patrickj@gmail.com>
2013-06-30 20:23:16 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'sqlite3'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :production do
+  gem 'binding_of_caller'
+  gem 'better_errors'
+end
+
+gem 'jquery-rails'
+
+gem 'backbone-on-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DropTask::Application.config.secret_token = 'af23d3c34a9dc46d3d6bf48e64400f7dda9fca32fdd29b10cd26444fbcd11c8d0b5c5125308aec531e3eaf4f1bb5258c5cd3bf5423f6ee4cdc958721417967c7'
