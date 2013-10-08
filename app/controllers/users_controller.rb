cd657dc54461ab3e8cb6e5d288709f15a71ef028
Patrick Slattery <slattery.patrickj@gmail.com>
2013-10-08 09:14:49 -0700

    add session to callback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      session[:token] = @user.set_session_token!
+      session[:token] = @user.session_token
       redirect_to root_url
     else
       render :new
